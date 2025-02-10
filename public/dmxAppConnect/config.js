f45cea6cc289e489f4589d73a8eb7cf3fdfae0c9
rbentes <rbntes@gmail.com>
2025-02-10 13:41:30 -0400

    Listagem de empresa, atualizacao de empresa selecionada no perfil de usuario, criar empresa/polo.

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,69 @@
+dmx.config({
+  "cadastrarpolo": {
+    "repeat1": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "id_empresa"
+        },
+        {
+          "type": "text",
+          "name": "nome_empresa"
+        },
+        {
+          "type": "text",
+          "name": "nome_fantasia"
+        },
+        {
+          "type": "text",
+          "name": "cnpj_empresa"
+        },
+        {
+          "type": "text",
+          "name": "telefone_empresa"
+        },
+        {
+          "type": "text",
+          "name": "celular_empresa"
+        },
+        {
+          "type": "text",
+          "name": "endereco_empresa"
+        },
+        {
+          "type": "text",
+          "name": "nome_polo"
+        },
+        {
+          "type": "text",
+          "name": "cep_polo"
+        },
+        {
+          "type": "text",
+          "name": "obs_endereco"
+        },
+        {
+          "type": "text",
+          "name": "status_empresa"
+        },
+        {
+          "type": "datetime",
+          "name": "criado_em"
+        },
+        {
+          "type": "datetime",
+          "name": "atualizado_em"
+        },
+        {
+          "type": "text",
+          "name": "Cidade"
+        },
+        {
+          "type": "text",
+          "name": "Estado"
+        }
+      ],
+      "outputType": "array"
+    }
+  }
+});
